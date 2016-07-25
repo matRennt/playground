88a835f57d49fc8f921022a8c2941ac721188b0b
matRennt <griese@ortec.org>
2016-07-25 16:20:57 +0200

    los!

--- my-app/src/main/java/org/ortec/app/App.java
+++ my-app/src/main/java/org/ortec/app/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Second Line" );
+        System.out.println( "Testing BitBucketPipeline right now!" );
     }
 }
