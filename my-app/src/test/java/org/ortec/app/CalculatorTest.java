16ebda1f42690af97bca719f555e99337f599e92
matRennt <griese@ortec.org>
2016-08-03 11:26:53 +0200

    CalculatorTest

--- my-app/src/test/java/org/ortec/app/CalculatorTest.java
+++ my-app/src/test/java/org/ortec/app/CalculatorTest.java
@@ -7,8 +7,16 @@ import org.junit.Test;
  * Created by ortecadmin on 01.08.2016.
  */
 public class CalculatorTest {
+
     @Test
     public void evaluatesExpression() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluate("1+2+3");
+        assertEquals(6, sum);
+    }
+
+    @Test
+    public void evaluatesExpression2() {
         Calculator calculator = new Calculator();
         int sum = calculator.evaluate("1+2+3");
         assertEquals(6, sum);
